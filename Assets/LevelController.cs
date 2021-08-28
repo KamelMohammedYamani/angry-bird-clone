c33a807fdd247d760d3e7d272825796a86183d29
KamelMohammedYamani <jm_yamani@esi.dz>
2021-08-28 19:20:38 +0100

    Initial commit

---
+++ Assets/LevelController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private Enemy[] _enemies;
+    private static int _nextLevelIndex =1;
+
+    private void OnEnable()
+    {
+      _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    void Update()
+    {
+      foreach (Enemy enemy in _enemies)
+      {
+        if (enemy != null)
+        return;
+      }
+
+      Debug.Log ("Bsahtek frr");
+
+      _nextLevelIndex ++;
+      string nextLevelName = "Level" + _nextLevelIndex ;
+      SceneManager.LoadScene(nextLevelName);
+    }
+}
